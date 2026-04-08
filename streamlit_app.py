289b1697cbf10a200360127fdc1e63d16ef9c36f
dhairyaparam <dhairya.param@iitgn.ac.in>
2026-04-08 12:37:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,6 +51,26 @@ with st.sidebar:
     st.markdown("---")
     st.caption("Values must be positive. ν must be < 0.5.")
 
+    st.sidebar.markdown("""
+    <div style="text-align: center; margin-top: 3rem;">
+        <p style="font-size: 0.85rem; color: #888;">
+            <br>
+            <b style="color: #555;">This tool determines the natrual frequencies for a plate of any arbitery shape for all possible modes</b><br>
+            For Custom set of parameters kindly refer to the Repository.
+        </p>
+    </div>
+    
+    <div style="text-align: center; margin-top: 3rem;">
+        <img src="https://iitgn.ac.in/assets/img/logo.png" width="100" style="margin-bottom: 10px;">
+        <p style="font-size: 0.85rem; color: #888;">
+            Developed in<br>
+            <b style="color: #555;">Center of Research Commercialization</b><br>
+            Indian Institute of Technology Gandhinagar
+        </p>
+    </div>
+""", unsafe_allow_html=True)
+
+
 # ──────────────────────────────────────────────────────────
 # Helper: shape + frequency plots
 # ──────────────────────────────────────────────────────────
